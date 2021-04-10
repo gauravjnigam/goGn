e568ad0565b70e50fc13d32d8cdf13078c2a8ad0
Gaurav Nigam <gauravjnigam@gmail.com>
2021-04-10 07:52:20 +0530

    updating client code

--- greet/greet-client/Client.go
+++ greet/greet-client/Client.go
@@ -2,11 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
+
 	"google.golang.org/grpc"
+
+	greetpb "greet/greetpb"
 )
 
 func main() {
 	fmt.Println("Hello client")
 
-	conn, err := grpc.Dial("localhost:50001", grpc.withInsecure())
+	conn, err := grpc.Dial("localhost:50001", grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalf("client is not able to connect : %v", err)
+	}
+
+	defer conn.Close()
+
+	c := greetpb.NewGreetServiceClient(conn)
+
+	fmt.Printf("created client : %f", c)
+
 }
