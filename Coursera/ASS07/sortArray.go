c6207b57438e906f219f2010d17136e065ddd8ff
Gaurav Nigam <gauravjnigam@gmail.com>
2021-04-18 23:31:59 +0530

    commit changes for merge sort with partitioned array

--- Coursera/ASS07/sortArray.go
+++ Coursera/ASS07/sortArray.go
@@ -4,8 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 /*
@@ -18,6 +20,8 @@ Each goroutine which sorts ¼ of the array should print the subarray that it wil
 When sorting is complete, the main goroutine should print the entire sorted list.
 
 */
+
+var wg sync.WaitGroup
 
 func main() {
 	fmt.Println("input integers (separated by space)...")
@@ -32,5 +36,68 @@ func main() {
 	for i, v := range inputSlice {
 		intSlice[i], _ = strconv.Atoi(v)
 	}
+	fmt.Printf("intput array : %v\n", intSlice)
 
+	const partitionSize int = 4
+
+	totalPartition := (len(intSlice) / partitionSize) + 1
+	wg.Add(totalPartition)
+	var mapOfSlice = make(map[int][]int)
+
+	for i := 0; i < len(intSlice); i++ {
+		index := i % totalPartition
+		if _, ok := mapOfSlice[index]; ok {
+			mapOfSlice[index] = append(mapOfSlice[index], intSlice[i])
+		} else {
+			mapOfSlice[index] = make([]int, 0)
+			mapOfSlice[index] = append(mapOfSlice[index], intSlice[i])
+		}
+
+	}
+
+	for j := 0; j < totalPartition; j++ {
+		go sortArr(mapOfSlice[j])
+	}
+
+	wg.Wait() // holding for all the partitioned slice sorting to complete
+
+	// merging slices now
+	for i := 1; i < totalPartition; i++ {
+		mapOfSlice[0] = mergeSortedArray(mapOfSlice[0], mapOfSlice[i])
+	}
+
+	fmt.Printf("sorted array : %v \n ", mapOfSlice[0])
+
+}
+
+func mergeSortedArray(left, right []int) (result []int) {
+	result = make([]int, len(left)+len(right))
+
+	i := 0
+	for len(left) > 0 && len(right) > 0 {
+		if left[0] < right[0] {
+			result[i] = left[0]
+			left = left[1:]
+		} else {
+			result[i] = right[0]
+			right = right[1:]
+		}
+		i++
+	}
+
+	for j := 0; j < len(left); j++ {
+		result[i] = left[j]
+		i++
+	}
+	for j := 0; j < len(right); j++ {
+		result[i] = right[j]
+		i++
+	}
+
+	return
+}
+
+func sortArr(arr []int) {
+	sort.Ints(arr)
+	wg.Done()
 }
